a506fc8cadcbf835dc892bec1051f90566d02013
bernat <b.pellicer@sapalomera.cat>
2021-10-04 20:35:04 +0200

    Login done
    Next Step sign up page

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useContext } from 'react';
 import { useHistory } from "react-router"
 import { Link } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
+import * as ROUTES from '../constants/routes';
 
 export default function Login(){
     const history = useHistory();
@@ -13,13 +14,18 @@ export default function Login(){
     const [error, setError] = useState('');
     const isInvalid = password === '' || emailAddress === '';
 
-    const handleLogin = () => {
-
-    }
-    
-    useEffect(() => {
-        document.title = 'Login - Instagram';
-    }, []);
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        try{
+            await firebase.auth().signInWithEmailAndPassword(emailAddress,password);
+            history.push(ROUTES.DASHBOARD)
+        }
+        catch(error){
+            setEmailAddress('');
+            setPassword('');
+            setError(error.message);
+        }
+    };
 
     const handleClick = () => {
         let togglePass = document.getElementById("togglePassword");
@@ -29,24 +35,30 @@ export default function Login(){
         password.setAttribute('type',type);
     };
 
+    useEffect(() => {
+        document.title = 'Login - Instagram';
+    }, []);
+
+  
+
     return(
-        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-            <div className="flex w-3/5">
+        <div className="container flex justify-center mx-auto max-w-screen-md items-center h-screen">
+            <div className="md:flex w-6/12 hidden">
                 <img src="/images/iphone-with-profile.jpg" alt="iPhone with profile"/>
             </div>
-            <div className="flex flex-col w-2/5">
+            <div className="flex flex-col md:w-6/12 px-5">
                 <div className="flex flex-col bg-white p-5 border-gray-300 border">
                     <h1 className="flex justify-center w-full">
                         <img src="images/logo.png" alt="Instagram" className="mt-2 mb-4 w-6/12"/>
                     </h1>
-                    {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+                    {error && <p className="mb-4 text-xs text-red-500">{error}</p>}
 
-                    <form onSubmit={handleLogin} method="POST">
+                    <form onSubmit={handleLogin} method="POST" className="pt-4">
                         <input 
                             aria-label="Enter your email address"
                             type="text"
                             placeholder="Email address"
-                            className="w-full text-sm text-gray-base mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2 focus:outline-none focus:border-blue-300"
+                            className="w-full text-sm text-gray-base mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-4 focus:outline-none focus:border-blue-300"
                             onChange={({target}) => setEmailAddress(target.value)}
                         />
                         <input 
@@ -54,7 +66,7 @@ export default function Login(){
                             type="password"
                             id="password"
                             placeholder="Password"
-                            className="w-full text-sm text-gray-base mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2 focus:outline-none focus:border-blue-300"
+                            className="w-full text-sm text-gray-base mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-4 focus:outline-none focus:border-blue-300"
                             onChange={({target}) => setPassword(target.value)}
                         />
                         <i className="bi bi-eye-slash" id="togglePassword" onClick={handleClick}></i>
@@ -67,12 +79,12 @@ export default function Login(){
                         </button>
                     </form>
                     <div className="mt-4 flex justify-center">
-                        <a className="text-sm text-center hover:text-blue-300" href="/">Forgot your password?</a>
+                        <a className="text-sm text-center under" href="/">Forgot your password?</a>
                     </div>
                 </div>
                 <div className="flex justify-center items-center flex-col w-full bg-white p-4 border-gray-300 border mt-4">
                     <p className="text-sm">Don't have an account?{' '}
-                        <Link to="/signup" className="font-bold text-blue-400">
+                        <Link to="/signup" className="under">
                             Sign up   
                         </Link>
                     </p>
---
+++ src/pages/signup.js
@@ -0,0 +1,9 @@
+import { useEffect, useState, useContext } from 'react';
+import { useHistory } from "react-router"
+import { Link } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+import * as ROUTES from '../constants/routes';
+
+export default function SignUp(){
+    
+}
